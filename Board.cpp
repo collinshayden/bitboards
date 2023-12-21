fc11093406cd6bd3cd772c17dd93e784e93c9c33
collinshayden <collinshayden171@gmail.com>
2023-12-21 14:09:27 -0500

    print_board function

--- Board.cpp
+++ Board.cpp
@@ -4,57 +4,32 @@
 
 #include "Board.h"
 
-void Board::set_starting_position() {
-    // set white pawns
-    for (int square = a2; square <= h2; square++) {
-        set_bit(white_pawns, square);
+
+void Board::print_board() {
+    for (int rank = 0; rank < 8; rank++) {
+        for (int file = 0; file < 8; file++) {
+            int square = rank * 8 + file;
+            int piece = -1; // init as empty
+
+            if (!file) {
+                printf(" %d  ", 8 - rank);
+            }
+            for (int bb_piece = white_pawn; bb_piece <= black_king; bb_piece++) {
+                if(get_bit(Board::piece_bitboards[bb_piece], square)) {
+                    piece = bb_piece;
+                }
+            }
+            printf("%s ", (piece == -1) ? "." : unicode_pieces[piece]);
+        }
+        printf("\n");
     }
-    // set black pawns
-    for (int square = a7; square <= h7; square++) {
-        set_bit(black_pawns, square);
-    }
-
-    // set white pieces
-    set_bit(white_rooks, a1);
-    set_bit(white_rooks, h1);
-    set_bit(white_knights, b1);
-    set_bit(white_knights, g1);
-    set_bit(white_bishops, c1);
-    set_bit(white_bishops, f1);
-    set_bit(white_queens, d1);
-    set_bit(white_kings, e1);
-
-    // set black pieces
-    set_bit(black_rooks, a8);
-    set_bit(black_rooks, h8);
-    set_bit(black_knights, b8);
-    set_bit(black_knights, g8);
-    set_bit(black_bishops, c8);
-    set_bit(black_bishops, f8);
-    set_bit(black_queens, d8);
-    set_bit(black_kings, e8);
-}
-
-// get all squares occupied by a white piece
-U64 Board::get_white_occupancy() {
-    return white_pawns | white_knights | white_bishops | white_rooks | white_queens | white_kings;
-}
-
-// get all squares occupied by a black piece
-U64 Board::get_black_occupancy() {
-    return black_pawns | black_knights | black_bishops | black_rooks | black_queens | black_kings;
-}
-
-// get all occupied squares
-U64 Board::get_total_occupancy() {
-    return get_white_occupancy() | get_black_occupancy();
+    printf("\n    a b c d e f g h\n");
 }
 
 int main() {
     Board board;
-    board.set_starting_position();
+    board.print_board();
 
-    U64 bitboard = board.get_total_occupancy();
-    print_bitboard(bitboard);
+
     return 0;
 }
--- Board.h
+++ Board.h
@@ -8,25 +8,23 @@
 #include "MoveGeneration.h"
 
 class Board {
-    U64 white_pawns = 0ULL;
-    U64 black_pawns = 0ULL;
-    U64 white_knights = 0ULL;
-    U64 black_knights = 0ULL;
-    U64 white_bishops = 0ULL;
-    U64 black_bishops = 0ULL;
-    U64 white_rooks = 0ULL;
-    U64 black_rooks = 0ULL;
-    U64 white_queens = 0ULL;
-    U64 black_queens = 0ULL;
-    U64 white_kings = 0ULL;
-    U64 black_kings = 0ULL;
+    // starting position
+    // pawn/knight/bishop/rook/queen/king, alternating white_pawn/black_pawn etc.
+    U64 piece_bitboards[12] = {0x00ff000000000000, 0x000000000000ff00, 0x4200000000000000, 0x0000000000000042,
+                               0x2400000000000000, 0x0000000000000024, 0x8100000000000000, 0x0000000000000081,
+                               0x0800000000000000, 0x0000000000000008, 0x1000000000000000, 0x0000000000000010,};
+    // occupancies, white, black, both
+    U64 occupancy_bitboards[3] = {0xffff000000000000, 0x000000000000ffff, 0xffff00000000ffff,};
+
+    int side = -1; // 0 white, 1 black
+
+    int enpassant = no_sq;
+
+    int castle;
 
     //    U64 generate_pseudo_legal_moves(U64 white_pawns, )
 public:
-    void set_starting_position();
-    U64 get_white_occupancy();
-    U64 get_black_occupancy();
-    U64 get_total_occupancy();
+    void print_board();
 };
 
 
