a4ae493365f689c0bdd1b86d37b9e0374d14f2e0
collinshayden <collinshayden171@gmail.com>
2023-12-23 23:21:22 -0500

    print pseudo legal moves

--- main.cpp
+++ main.cpp
@@ -7,7 +7,12 @@
 int main() {
     fill_attack_tables();
     Board board;
-    board.legal_moves();
+    std::vector<int> legal_moves = board.legal_moves();
+
+    for (int legal_move : legal_moves) {
+        printf("%s%s\n", square_to_cord[get_move_source(legal_move)], square_to_cord[get_move_target(legal_move)]);
+    }
+
 
     return 0;
 }
