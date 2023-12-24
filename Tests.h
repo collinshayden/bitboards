5cb62431950c1b21f431c16ac8bfe42e13c74b4c
collinshayden <collinshayden171@gmail.com>
2023-12-23 23:20:21 -0500

    created test suite

---
+++ Tests.h
@@ -0,0 +1,17 @@
+//
+// Created by Hayden Collins on 12/23/23.
+//
+
+#ifndef BITBOARDS_TESTS_H
+#define BITBOARDS_TESTS_H
+
+#include "MoveGeneration.h"
+#include "Board.h"
+
+void pawnsTestOne();
+void knightsTestOne();
+void bishopsTestOne();
+void queensTestOne();
+void kingsTestOne();
+
+#endif //BITBOARDS_TESTS_H
