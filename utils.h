e1e48e4675db5ca14da7d59f579b076dc70f8b62
collinshayden <collinshayden171@gmail.com>
2023-12-23 23:20:33 -0500

    simplified piece enum

--- utils.h
+++ utils.h
@@ -14,20 +14,14 @@ enum {
     white, black, all
 };
 
-// pieces, alternating, so we can do white_pawn + side_to_move to index black pieces
+// pieces, incrementing by 2, so we can do pawn + side_to_move to index black pawns
 enum {
-    white_pawn,
-    black_pawn,
-    white_knight,
-    black_knight,
-    white_bishop,
-    black_bishop,
-    white_rook,
-    black_rook,
-    white_queen,
-    black_queen,
-    white_king,
-    black_king,
+    pawn,
+    knight = 2,
+    bishop = 4,
+    rook = 6,
+    queen = 8,
+    king = 10,
 };
 
 // castling rights
